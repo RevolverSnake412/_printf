5e103bc13a7fd5be6b34604e22bf507288266b8b
Bello Osagie <osagienoah04@gmail.com>
2023-07-23 03:39:48 -0700

    commit changes
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- _printf.c
+++ _printf.c
@@ -1,52 +1,53 @@
+#include <unistd.h>
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
+
+int count;
+
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
- * Return: number of characters printed
+ * _printf: Similar to printf standard function
+ *
+ * @format: The type the user will choose.
+ *
+ * Return:
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
+	int counter = 0;
+	const char *p = format;
+	va_list args;
+	va_start(args, format);
+	while (*p)
 	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
-
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
+		if (*p == '%')
 		{
-			if (format[i] == '%')
+			p++;
+
+			switch (*p)
 			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
+			case 'c':
+				_printf_c((char)va_arg(args, int));
+				break;
+			case 's':
+				_printf_s(va_arg(args, char *));
+				break;
+			case '%':
+				write(1, "%", 1);
+				counter++;
+				break;
+			default:
+				write(1, p, 1);
+				counter++;
+				break;
 			}
 		}
-		va_end(args);
-		return (count);
+		else
+		{
+			write(1, p, 1);
+			counter++;
+		}
+		p++;
 	}
-	return (-1);
+	va_end(args);
+	return (counter);
 }
--- functions.c
+++ functions.c
@@ -1,100 +1,38 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-/**
- * print_c - prints character
- * @args: character argument
- * Return: number of characters
- */
-int print_c(va_list args)
-{
-	int c;
+#include "unistd.h"
 
-	c = va_arg(args, int);
-	return (_putchar(c));
-}
-/**
- * print_s - prints a string
- * @args: string  argument
- * Return: number of characters
- */
-int print_s(va_list args)
-{
-	int i, count = 0;
-	char *str;
+int count = 0;
 
-	i = 0;
-	str = va_arg(args, char *);
-	if (str == NULL)
-		str = "(null)";
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-		count++;
-	}
-	return (count);
-}
 /**
- * print_percent - pass the percent sing
- * @args: string  argument
- * Return: return the percent sing
+ * _putchar_c: For outputing single character.
+ * @content: Provided by user.
  *
+ * Return: 0 on success
  */
-int print_percent(va_list args)
+int _printf_c(char content)
 {
-	char *str;
+	char c = content;
 
-	str = "%";
-	if (va_arg(args, int) == *str)
-	{
-		return (*str);
-	}
-	return (*str);
+	write(1, &c, 1);
+	count++;
+	return (0);
 }
 
 /**
- * print_d - prints a decimal
- * @args: decimal argument
- * Return: counter
+ * _putchar_s: For outputing a string.
+ * @content: Provided by user.
+ *
+ * Return: 0 on success
  */
-int print_d(va_list args)
+int _printf_s(char *content)
 {
+	char *str = content;
+	int len = 0;
 
-	unsigned int absolute, aux, countnum, count;
-	int n;
+	while (str[len])
+		len++;
 
-	count = 0;
-	n = va_arg(args, int);
-	if (n < 0)
-	{
-		absolute = (n * -1);
-		count += _putchar('-');
-	}
-	else
-		absolute = n;
-
-	aux = absolute;
-	countnum = 1;
-	while (aux > 9)
-	{
-		aux /= 10;
-		countnum *= 10;
-	}
-	while (countnum >= 1)
-	{
-		count += _putchar(((absolute / countnum) % 10) + '0');
-		countnum /= 10;
-	}
-	return (count);
-}
-/**
- * print_i - prints integer
- * @args: integer argument
- * Return: the decimal function
- */
-
-int print_i(va_list args)
-{
-	return (print_d(args));
+	write(1, str, len);
+	count += len;
+	return (0);
 }
