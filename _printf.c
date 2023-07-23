f8f82e6c2d96a83db7944170f4c73fc40fcee35a
Bello Osagie <osagienoah04@gmail.com>
2023-07-23 03:50:46 -0700

    commit changes
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- _printf.c
+++ _printf.c
@@ -11,41 +11,41 @@
  */
 int _printf(const char *format, ...)
 {
-    int counter = 0;
-    const char *p = format;
-    va_list args;
-    va_start(args, format);
-    while (*p)
-    {
-        if (*p == '%')
-        {
-            p++;
+	int counter = 0;
+	const char *p = format;
+	va_list args;
+	va_start(args, format);
+	while (*p)
+	{
+		if (*p == '%')
+		{
+			p++;
 
-            switch (*p)
-            {
-            case 'c':
-                counter += _printf_c((char)va_arg(args, int));
-                break;
-            case 's':
-                counter += _printf_s(va_arg(args, char *));
-                break;
-            case '%':
-                write(1, "%", 1);
-                counter++;
-                break;
-            default:
-                write(1, p, 1);
-                counter++;
-                break;
-            }
-        }
-        else
-        {
-            write(1, p, 1);
-            counter++;
-        }
-        p++;
-    }
-    va_end(args);
-    return (counter);
+			switch (*p)
+			{
+			case 'c':
+				counter += _printf_c((char)va_arg(args, int));
+				break;
+			case 's':
+				counter += _printf_s(va_arg(args, char *));
+				break;
+			case '%':
+				write(1, "%", 1);
+				counter++;
+				break;
+			default:
+				write(1, p, 1);
+				counter++;
+				break;
+			}
+		}
+		else
+		{
+			write(1, p, 1);
+			counter++;
+		}
+		p++;
+	}
+	va_end(args);
+	return (counter);
 }
--- functions.c
+++ functions.c
@@ -1,36 +1,38 @@
 #include "main.h"
 #include "unistd.h"
 
+int count = 0;
+
 /**
- * _printf_c: For outputting a single character.
- * @content: Provided by the user.
+ * _putchar_c: For outputing single character.
+ * @content: Provided by user.
  *
  * Return: 0 on success
  */
 int _printf_c(char content)
 {
-    char c = content;
+	char c = content;
 
-    write(1, &c, 1);
-    return (1);
+	write(1, &c, 1);
+	count++;
+	return (0);
 }
 
 /**
- * _printf_s: For outputting a string.
- * @content: Provided by the user.
+ * _putchar_s: For outputing a string.
+ * @content: Provided by user.
  *
- * Return: the length of the string printed
+ * Return: 0 on success
  */
 int _printf_s(char *content)
 {
-    if (content == NULL)
-        return (write(1, "(null)", 6));
-    
-    int len = 0;
+	char *str = content;
+	int len = 0;
 
-    while (content[len])
-        len++;
+	while (str[len])
+		len++;
 
-    write(1, content, len);
-    return (len);
+	write(1, str, len);
+	count += len;
+	return (0);
 }
