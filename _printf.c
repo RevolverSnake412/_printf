ba3244e91bd56e1f6c8c113c6792eaf67fdd09f1
Bello Osagie <osagienoah04@gmail.com>
2023-07-23 03:07:58 -0700

    commit changes
    Co-authored-by: Bello Osagie <osagienoah04@gmail.com>

--- _printf.c
+++ _printf.c
@@ -1,53 +1,65 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
-int count;
-
-/**
- * _printf: Similar to printf standard function
- *
- * @format: The type the user will choose.
- *
- * Return:
-*/
 int _printf(const char *format, ...)
 {
-	int counter = 0;
-	const char *p = format;
 	va_list args;
 	va_start(args, format);
-	while (*p)
-	{
-		if (*p == '%')
-		{
-			p++;
+	int chars_printed = 0;
 
-			switch (*p)
-			{
-			case 'c':
-				_printf_c((char)va_arg(args, int));
-				break;
-			case 's':
-				_printf_s(va_arg(args, char*));
-				break;
-			case '%':
-				write(1, "%", 1);
-				counter++;
-				break;
-			default:
-				write(1, p, 1);
-				counter++;
-				break;
-			}
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			putchar(*format);
+			chars_printed++;
 		}
 		else
 		{
-			write(1, p, 1);
-			counter++;
+			format++; // Move past the '%'
+			switch (*format)
+			{
+			case 'c':
+				putchar(va_arg(args, int));
+				chars_printed++;
+				break;
+			case 's':
+			{
+				const char *str = va_arg(args, const char *);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					chars_printed++;
+				}
+			}
+			break;
+			case '%':
+				putchar('%');
+				chars_printed++;
+				break;
+			default:
+				putchar('%');
+				putchar(*format);
+				chars_printed += 2;
+				break;
+			}
 		}
-		p++;
-		}
-		va_end(args);
-		return (counter);
+		format++;
+	}
+
+	va_end(args);
+	return chars_printed;
+}
+
+int main()
+{
+	int num = 42;
+	char ch = 'X';
+	const char *str = "Hello, world!";
+
+	int printed_chars = _printf("This is a test: %d %c %s %%\n", num, ch, str);
+	printf("\nNumber of characters printed: %d\n", printed_chars);
+
+	return 0;
 }
--- main.h
+++
@@ -1,8 +0,0 @@
-#ifndef THEADER_H
-#define THEADER_H
-
-int _printf(const char *format, ...);
-int _printf_c(char content);
-int _printf_s(char *content);
-
-#endif
